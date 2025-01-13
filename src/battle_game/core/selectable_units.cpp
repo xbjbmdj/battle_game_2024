0eee9192461bc9ff5d499cd509cb577ab6a233eb
xbjbmdj <72010875+xbjbmdj@users.noreply.github.com>
2025-01-13 23:20:01 +0800

    Update selectable_units.cpp

--- src/battle_game/core/selectable_units.cpp
+++ src/battle_game/core/selectable_units.cpp
@@ -23,6 +23,7 @@ void GameCore::GeneratePrimaryUnitList() {
    */
   ADD_SELECTABLE_UNIT(unit::Tank);
   ADD_SELECTABLE_UNIT(unit::zmdzg1);
+  ADD_SELECTABLE_UNIT(unit::Shift);
 
   unit.reset();
 }
